0ce2a8bdf9a4f26defeb4be65658b2de6ca3516e
Giordanna <giordanna.gregoriis@terceirizado.febrafar.com.br>
2023-09-19 13:20:43 -0300

    style adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,17 +65,19 @@
 	};
 </script>
 
-<main class="w-full max-w-screen-lg mx-auto mt-3">
+<main class="w-full max-w-screen-lg px-3 md:px-0 mx-auto mt-3 md:mt-5">
 	<h1 class="font-bold text-xl">Vimeo Player iOS Issue</h1>
 
 	<p class="text-slate-500 my-3">Add the vimeo ID or URL below:</p>
 
-	<fielset class="inline-flex items-center space-x-3 mb-3">
-		<input type="text" bind:value={vimeoId} class="rounded-lg" />
+	<fielset
+		class="inline-flex flex-col w-full md:flex-row space-x-0 md:space-y-0 space-y-3 items-center md:space-x-3 mb-3"
+	>
+		<input type="text" bind:value={vimeoId} class="rounded-lg w-full md:w-auto" />
 		<button
 			on:click={reloadPlayer}
 			disabled={vimeoId === ''}
-			class="bg-emerald-600 active:bg-emerald-700 transition-all disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-emerald-600/50 text-white rounded-lg font-bold p-2"
+			class="bg-emerald-600 w-full md:w-auto active:bg-emerald-700 transition-all disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-emerald-600/50 text-white rounded-lg font-bold p-2"
 		>
 			Reload player
 		</button>
